8d541009df533786b84819d8b12b1f5da8bcc2ff
Javier Jiménez-Alfaro Hacha <javilin7967@gmail.com>
2024-01-30 20:04:24 +0100

    firstCommit

---
+++ app/src/main/java/com/hachatml/cocktaillooker/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.hachatml.cocktaillooker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hachatml.cocktaillooker.ui.theme.CocktailLookerTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CocktailLookerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(text = "¿Nos bebemos algo, máquina?")
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(text = "Ofréceme")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
